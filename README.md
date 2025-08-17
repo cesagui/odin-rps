58e14da9de1088e297080f6a457d039b5d4143cc
Cesar Aguirre <cesar.aguirre.work@gmail.com>
2025-08-17 15:28:36 -0700

    fix: added on-page interactivity for game

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # odin-rps
 
-This is a basic console Rock, Paper, Scissors game made in Javascript. The game works using window dialogs, and in order to run, load the webpage and run the playGame() method in the terminal.
+This is a basic console Rock, Paper, Scissors game made in Javascript. The game works using the buttons on the webpage.
--- javascript.js
+++ javascript.js
@@ -13,61 +13,113 @@ function getComputerChoice(){
     }
 }
 
-function getHumanChoice(){
-    let answer = prompt('Enter your choice: "rock", "paper", or "scissors"');
-    return answer.toLowerCase();
-
-}
-
 function playRound(playerChoice, computerChoice){
     if (playerChoice == computerChoice){
-        console.log("Ah! We both chose "+ playerChoice +"!");
+        resultElement.textContent = "Ah! We both chose "+ playerChoice +"!";
     } else {
         if (playerChoice == 'rock'){
             if (computerChoice == 'paper'){
                 computerScore++;
-                console.log("Oh no! The computer's "+ computerChoice +" beat my " + playerChoice + "!");
+                resultElement.textContent = "Oh no! The computer's "+ computerChoice +" beat my " + playerChoice + "!";
             }
             if (computerChoice == 'scissors'){
                 playerScore++;
-                console.log("Haha! My "+ playerChoice +" beat the computer's " + computerChoice + "!");
+                resultElement.textContent = "Haha! My "+ playerChoice +" beat the computer's " + computerChoice + "!";
             }
         } else if (playerChoice == 'paper'){
             if (computerChoice == 'rock'){
                 playerScore++;
-                console.log("Haha! My "+ playerChoice +" beat the computer's " + computerChoice + "!");
+                resultElement.textContent = "Haha! My "+ playerChoice +" beat the computer's " + computerChoice + "!";
             }
             if (computerChoice == 'scissors'){
                 computerScore++;
-                console.log("Oh no! The computer's "+ computerChoice +" beat my " + playerChoice + "!");
+                resultElement.textContent = "Oh no! The computer's "+ computerChoice +" beat my " + playerChoice + "!";
             }
         } else if (playerChoice == 'scissors'){
             if (computerChoice == 'rock'){
                 computerScore++;
-                console.log("Oh no! The computer's "+ computerChoice +" beat my " + playerChoice + "!");
+                resultElement.textContent = "Oh no! The computer's "+ computerChoice +" beat my " + playerChoice + "!";
             }
             if (computerChoice == 'paper'){
                 playerScore++;
-                console.log("Haha! My "+ playerChoice +" beat the computer's " + computerChoice + "!");
+                resultElement.textContent = "Haha! My "+ playerChoice +" beat the computer's " + computerChoice + "!";
             }
         }
     }
-    console.log("Scores\nPlayer: " + playerScore + "\tComputer: " + computerScore);
+    // console.log("Scores\nPlayer: " + playerScore + "\tComputer: " + computerScore);
 }
+
+function updateScoreLabels(){
+    hsElement.textContent = playerScore;
+    csElement.textContent = computerScore;
+}
+
+function appendVictoryMessage(){
+    if (playerScore == 3){
+        resultElement.textContent += " Human wins! We beat the machines!";    
+    }
+    if (computerScore == 3){
+        resultElement.textContent += " Computer wins! My job is cooked by AI!";    
+    }
+}
+
+function playGame(){
+    updateScoreLabels();
+        
+    document.addEventListener('click', function (event) {
+        if (playerScore > 2 || computerScore > 2){
+            playerScore = 0;
+            computerScore = 0;
+        }
+        let target = event.target;
+
+        switch (target.id){
+            case ("rock-button"):
+                h = "rock";
+                playRound(h, getComputerChoice());
+                appendVictoryMessage();
+                break;
+            case ("paper-button"):
+                h = "paper"
+                playRound(h, getComputerChoice());;   
+                appendVictoryMessage();
+                break;
+            case ("scissors-button"):
+                h = "scissors";   
+                playRound(h, getComputerChoice());
+                appendVictoryMessage();   
+                break;
+        }
+
+        updateScoreLabels();
+    });
+}
+
 
 let playerScore = 0;
 let computerScore = 0;
 
-function playGame(){
+let hsElement = document.querySelector(".scores #human-score p");
+let csElement = document.querySelector(".scores #cpu-score p");
+let resultElement = document.querySelector(".results p");
 
-    for (let i = 0; i < 5; i++){
-        let h = getHumanChoice();
-        let c = getComputerChoice();
-        if (h === ""){
-            break;
-        }
-        playRound(h, c);
-    }
-}
+updateScoreLabels();
+playGame();
 
+// function playGame(){
+
+//     for (let i = 0; i < 5; i++){
+//         let h = getHumanChoice();
+//         let c = getComputerChoice();
+//         if (h === ""){
+//             break;
+//         }
+//         playRound(h, c);
+//     }
+// }
+
+// function getHumanChoice(){
+//     let answer = prompt('Enter your choice: "rock", "paper", or "scissors"');
+//     return answer.toLowerCase();
+// }
 
